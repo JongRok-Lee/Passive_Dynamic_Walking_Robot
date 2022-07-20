8fcfbd15b422b59a5285c0405a46b3d5ff1c1872
JongRok Lee <99572329+JongRok-Lee@users.noreply.github.com>
2022-07-20 20:48:13 +0900

    Create event_fn.m

---
+++ comapss-gait model/upper body model/event_fn.m
@@ -0,0 +1,14 @@
+function [valu, isterm, direc] = event_fn(t,x)
+% JongRok Lee
+
+global phi_init l
+
+x_end_thsw = -l*sin(x(1)+phi_init) + l*sin(x(3)+phi_init);
+y_end_thsw =  l*cos(x(1)+phi_init) - l*cos(x(3)+phi_init);
+
+
+scuffing = (x_end_thsw <= 0.2);
+
+valu   = y_end_thsw;
+isterm = ~scuffing;
+direc  = -1;      
